bf7d81f0db5902881883d9e493321e89a773b1e0
levius_codeWell <taskaleg@gmail.com>
2023-12-17 15:20:07 -0500

    issue with sheet access

--- api.js
+++ api.js
@@ -1,9 +1,6 @@
 const { google } = require('googleapis');
-
-// Load credentials from the JSON file you downloaded
 const credentials = require('./keys.json');
 
-// Configure the Google Sheets API
 const sheets = google.sheets({
     version: 'v4',
     auth: new google.auth.JWT(
@@ -15,25 +12,40 @@ const sheets = google.sheets({
 });
 
 module.exports = {
-  submitToGoogleSheet: (req, res) => {
+  submitToGoogleSheet: async (req, res) => {
     const name = req.body.name;
 
-    // Append data to the Google Sheet
-    sheets.spreadsheets.values.append({
-      spreadsheetId: '1vk9U8D8WY3EvQcqSVFPE9mzg5Wz1XjWd8vh5HsUMva8',
-      range: 'Sheet1', // Update with your sheet name
-      valueInputOption: 'USER_ENTERED',
-      resource: {
-        values: [[name]],
-      },
-    }, (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error submitting data to Google Sheet');
-      } else {
-        console.log('Data submitted to Google Sheet:', result.data);
-        res.send('Data submitted successfully!');
-      }
-    });
+    try {
+      // Create a new spreadsheet
+      const spreadsheetResponse = await sheets.spreadsheets.create({
+        resource: {
+          properties: {
+            title: `Form Responses - ${Date.now()}`,
+          },
+        },
+      });
+
+      // Extract the spreadsheetId from the response
+      const spreadsheetId = spreadsheetResponse.data.spreadsheetId;
+
+      // Append data to the new Google Sheet
+      const appendResult = await sheets.spreadsheets.values.append({
+        spreadsheetId: spreadsheetId,
+        range: 'Sheet1', // Update with your sheet name
+        valueInputOption: 'USER_ENTERED',
+        resource: {
+          values: [[name]],
+        },
+      });
+
+      console.log('Data submitted to Google Sheet:', appendResult.data);
+
+      // Redirect to the Google Sheets document
+      const googleSheetsUrl = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/edit`;
+      res.redirect(googleSheetsUrl);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error submitting data to Google Sheet');
+    }
   },
 };
